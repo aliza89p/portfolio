649c39aa7f05b418f2bf0ed920de01460a1fef56
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-11 21:42:17 -0700

    added attributes and append to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,31 @@
 var projects = [];
 
-function Project (ops){
-  this.title = ops.title;
-  this.when = ops.when;
-  this.about = ops.about;
-  this.infoUrl = ops.infoUrl;
+function Project (obj){
+  this.title = obj.title;
+  this.when = obj.when;
+  this.about = obj.about;
+  this.infoUrl = obj.infoUrl;
+  this.class = obj.class;
+  this.id = obj.id;
 };
 
 Project.prototype.toHtml = function(){
-  var $newProject = $('projects.template').clone();
+  var $newProject = $('article.template').clone();
+  $newProject.attr('class', this.class);
+  $newProject.attr('id', this.id);
   $newProject.find('h1').html(this.title);
   $newProject.find('p').html(this.when);
   $newProject.find('p').html(this.about);
   $newProject.find('a').attr('href', this.infoUrl);
 
-  // $newProject.append('<article>');
-  //
-  // $newProject.removeClass('template');
+  $newProject.removeClass('template');
   return $newProject;
 };
+
+projectData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a){
+  $('#projects').append(a.toHtml());
+});
