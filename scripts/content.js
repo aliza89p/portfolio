c635389f9e1002d7715f2114d06075852951b2f7
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-11 22:29:21 -0700

    updated content

--- scripts/content.js
+++ scripts/content.js
@@ -13,7 +13,7 @@ var projectData = [
     class: 'event',
     id: 'app-a-thon',
     when: 'November, 2015',
-    about: '<p>As a founding Google Code Corps member, I helped pilot <a href="http://cs-first.com">Google CS First</a> at Boys & Girls Clubhouses and school sites across the Portland Metropolitan area. Part of my assignment was to plan and implement a hackathon event.</p></p>On November 7th, 2015, our staff drove 45 Club youth from sites across the city to the Northeast Portland Boys & Girls Club. I recruited and trained 14 skilled volunteers who led small teams through the process of brainstorming, wireframing, and programming an Android app. We served two hearty meals, held a project fair, and provided our youth with certificates and resources on continuing with computer science. </p>',
+    about: '<p>As a founding Google Code Corps member, I helped pilot <a href="http://cs-first.com">Google CS First</a> at Boys & Girls Clubhouses and school sites across the Portland Metropolitan area. Part of my assignment was to plan and implement a hackathon event.</p></p>On the day of the event, staff drove 45 youth from Club sites across the city to the Northeast Portland Boys & Girls Club. I recruited and trained 14 skilled volunteers who led small teams through the process of brainstorming, wireframing, and programming an Android app. We served two hearty meals, held a project fair, and provided our youth with certificates and resources on continuing with computer science. </p>',
     infoUrl: 'https://www.flickr.com/photos/131466850@N04/sets/72157661000662512'
   }
 ];
