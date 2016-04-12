8d978861b58c443d429aeec700d1c2bb99bd8ef5
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-11 21:21:24 -0700

    building object constructor and method

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,21 @@
-console.log('testing');
+var projects = [];
+
+function Project (ops){
+  this.title = ops.title;
+  this.when = ops.when;
+  this.about = ops.about;
+  this.infoUrl = ops.infoUrl;
+};
+
+Project.prototype.toHtml = function(){
+  var $newProject = $('projects.template').clone();
+  $newProject.find('h1').html(this.title);
+  $newProject.find('p').html(this.when);
+  $newProject.find('p').html(this.about);
+  $newProject.find('a').attr('href', this.infoUrl);
+
+  // $newProject.append('<article>');
+  //
+  // $newProject.removeClass('template');
+  return $newProject;
+};
