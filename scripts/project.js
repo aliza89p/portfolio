7fb94a3bee25f81e1889ec249e562ff79541c0d7
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-19 16:01:45 -0700

    wrapped in IFFE and created dynamic parameter for fetchAllProjects

--- scripts/project.js
+++ scripts/project.js
@@ -1,48 +1,51 @@
-function Project (obj){
-  for (key in obj) this[key] = obj[key];
-};
+(function(module){
+  function Project (obj){
+    for (key in obj) this[key] = obj[key];
+  };
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function(){
-  var template = Handlebars.compile($('#project-template').html());
+  Project.prototype.toHtml = function(){
+    var template = Handlebars.compile($('#project-template').html());
 
-  return template(this);
-};
+    return template(this);
+  };
 
-Project.loadAllProjects = function(dataPassedIn){
-  dataPassedIn.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
+  Project.loadAllProjects = function(dataPassedIn){
+    dataPassedIn.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
 
-Project.fetchAllProjects = function(){
-  if(localStorage.projectContent){
-    console.log(localStorage.projectContent);
-    $.ajax({
-      type: 'HEAD',
-      url: '../data/projectContent.json',
-      success:function (data, message, xhr){
-        var eTag = xhr.getResponseHeader('eTag');
-        console.log(eTag);
-        if (eTag === localStorage.eTag){
-          Project.loadAllProjects(JSON.parse(localStorage.projectContent));
-          projectView.initializeIndex();
-        }else{
-          localStorage.eTag = eTag;
-          $.getJSON('../data/projectContent.json', function(data){
-            Project.loadAllProjects(data);
-            localStorage.projectContent = JSON.stringify(data);
-            projectView.initializeIndex();
-          });
+  Project.fetchAllProjects = function(callBackFunction){
+    if(localStorage.projectContent){
+      console.log(localStorage.projectContent);
+      $.ajax({
+        type: 'HEAD',
+        url: '../data/projectContent.json',
+        success:function (data, message, xhr){
+          var eTag = xhr.getResponseHeader('eTag');
+          console.log(eTag);
+          if (eTag === localStorage.eTag){
+            Project.loadAllProjects(JSON.parse(localStorage.projectContent));
+            callBackFunction();
+          }else{
+            localStorage.eTag = eTag;
+            $.getJSON('../data/projectContent.json', function(data){
+              Project.loadAllProjects(data);
+              localStorage.projectContent = JSON.stringify(data);
+              callBackFunction();
+            });
+          }
         }
-      }
-    });
-  }else{
-    $.getJSON('../data/projectContent.json', function(data){
-      Project.loadAllProjects(data);
-      localStorage.projectContent = JSON.stringify(data);
-      projectView.initializeIndex();
-    });
-  }
-};
+      });
+    }else{
+      $.getJSON('../data/projectContent.json', function(data){
+        Project.loadAllProjects(data);
+        localStorage.projectContent = JSON.stringify(data);
+        projectView.initializeIndex();
+      });
+    }
+  };
+  module.Project = Project;
+})(window);
