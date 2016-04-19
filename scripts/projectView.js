b12ace1ad3729e38ccd0502b532197d8c0849944
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-19 16:01:58 -0700

    wrapped in IFFE

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,17 +1,20 @@
-var projectView = {};
+(function(module){
+  var projectView = {};
 
-projectView.handleNavBar = function(){
-  $('.nav-bar').on('click', '.page', function(){
-    $('.page-content').hide();
-    $('#' + $(this).data('content')).show();
-  });
-  $('.nav-bar .page:first').click();
-};
+  projectView.handleNavBar = function(){
+    $('.nav-bar').on('click', '.page', function(){
+      $('.page-content').hide();
+      $('#' + $(this).data('content')).show();
+    });
+    $('.nav-bar .page:first').click();
+  };
 
 
-projectView.initializeIndex = function(){
-  Project.all.forEach(function(a){
-    $('#projects').append(a.toHtml());
-  });
-  projectView.handleNavBar();
-};
+  projectView.initializeIndex = function(){
+    Project.all.forEach(function(a){
+      $('#projects').append(a.toHtml());
+    });
+    projectView.handleNavBar();
+  };
+  module.projectView = projectView;
+})(window);
