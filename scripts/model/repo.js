8fe85de55fa3292dc464545e72c471adb096c9b5
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-27 13:59:08 -0700

    changed username to aliza89p

--- scripts/model/repo.js
+++ scripts/model/repo.js
@@ -6,7 +6,7 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.get('/github/users/codefellows/repos' +
+    $.get('/github/users/aliza89p/repos' +
           '?per_page=10' +
           '&sort=updated')
     .done(function(data) {
