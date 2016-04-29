24eb61fe63a217a0cfbc9f698a3d28db682aa0c2
Aliza Pilisuk <apilisuk@gmail.com>
2016-04-28 17:02:20 -0700

    removed commented out commit code from repo.js

--- scripts/model/repo.js
+++ scripts/model/repo.js
@@ -1,7 +1,5 @@
 (function(module) {
   var repos = {};
-  commits = 0;
-  totalCommits = [];
 
   repos.all = [];
 
@@ -14,26 +12,6 @@
     })
   .done(callback);
   };
-
-  // repos.all.map(function(repo) {
-  //   $.ajax({
-  //     url: 'https://api.github.com/repos/aliza89p/' + repo.name + '/stats/participation',
-  //     type: 'GET',
-  //     headers: {
-  //       'Authorization': 'token ' + githubToken },
-  //     success: function(data){
-  //       if (data.owner.length > 0) {
-  //         totalCommits.push(commits = commits + data.owner.reduce(function(a, b){
-  //           return a + b;
-  //         }));
-  //       }
-  //       // totalCommits.reduce(function(a, b) {
-  //       //   return a + b;
-  //       // });
-  //       callback();
-  //     }
-  //   });
-  // });
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
