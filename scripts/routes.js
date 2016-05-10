effbb0255d99a02b764a4d764f2728a8d7a78581
Aliza Pilisuk <apilisuk@gmail.com>
2016-05-09 20:23:12 -0700

    added / before routes and hrefs

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,6 +1,6 @@
 page('/', homeController.index);
 page('/projects', projectController.index);
-page('projects/:category', projectController.displayByCategory, projectController.index);
+page('/projects/:category', projectController.displayByCategory, projectController.index);
 page('/about', aboutController.index);
 
 page();
