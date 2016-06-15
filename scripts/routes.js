358cea60b5c8e23befd51346bc4bc7087361edde
Aliza Pilisuk <apilisuk@gmail.com>
2016-06-14 17:28:49 -0700

    removed commented out code

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,6 +1,5 @@
 page('/', homeController.index);
 page('/projects', projectController.index);
-// page('/projects/:category', projectController.index, projectController.displayByCategory);
 page('/about', aboutController.index);
 page('/contact', contactController.index);
 
