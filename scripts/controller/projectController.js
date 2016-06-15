62ff86852740b6085007a18693d2c4a58652d775
Aliza Pilisuk <apilisuk@gmail.com>
2016-06-14 17:27:06 -0700

    removed commented out code and unused images

--- scripts/controller/projectController.js
+++ scripts/controller/projectController.js
@@ -5,23 +5,9 @@
     $('#home').hide();
     $('#about').hide();
     $('#contact').hide();
-    Project.fetchAllProjects(projectView.initializeProjects);
     $('#projects').show();
-    // next();
+    Project.fetchAllProjects(projectView.initializeProjects);
   };
-
-  // projectController.displayByCategory = function(ctx) {
-  //   $('#category-filter').on('change', function() {
-  //     if ($(this).val()) {
-  //       $('#projects article').hide();
-  //       $('article[data-category="' + $(this).val() + '"]').show();
-  //     }
-  //     else {
-  //       $('article').show();
-  //       $('article.template').hide();
-  //     }
-  //   });
-  // };
 
   module.projectController = projectController;
 })(window);
