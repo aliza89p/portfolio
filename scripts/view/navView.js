805f6502e2734e7048aa2d02b0889e7bc13fa66f
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-25 21:57:47 -0700

    main section moves down when hamburger menu is clicked

--- scripts/view/navView.js
+++ scripts/view/navView.js
@@ -2,14 +2,17 @@ $(function() {
   $('.icon-menu').on('click', function(){
     if($('.nav-items').css('display') === 'none'){
       $('.nav-items').show();
+      $('main').css('padding-top', '140px');
     }else{
       $('.nav-items').hide();
+      $('main').css('padding-top', '');
     }
   });
 
   $('.nav-items').on('click', function() {
     if ($('html').width() < 850){
       $('.nav-items').hide();
+      $('main').css('padding-top', '');
     }
   });
 
