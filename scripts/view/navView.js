788cbd6e1152d68c4cd92d8da3e48679d47825f3
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-26 01:05:45 -0700

    handle nav in resizing browser

--- scripts/view/navView.js
+++ scripts/view/navView.js
@@ -19,6 +19,17 @@ $(function() {
   $(window).resize(function(){
     if ($('html').width() > 850){
       $('.nav-items').show();
+      $('main').css('padding-top', '');
     };
   });
+
+  $(window).resize(function(){
+    if ($('html').width() < 850 && $('.nav-items').show()){
+      $('main').css('padding-top', '140px');
+    }
+    if ($('html').width() < 850 && $('.nav-items').hide()){
+      $('main').css('padding-top', '');
+    }
+  });
+
 });
