9aae6f9ccd42686fa9a080e4376c21203db617d2
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-25 21:27:35 -0700

    fixed nav bar

--- scripts/view/navView.js
+++ scripts/view/navView.js
@@ -1,21 +1,21 @@
 $(function() {
   $('.icon-menu').on('click', function(){
     if($('.nav-items').css('display') === 'none'){
-      $('.nav-items').css('display', '');
+      $('.nav-items').show();
     }else{
-      $('.nav-items').css('display', 'none');
+      $('.nav-items').hide();
     }
   });
 
   $('.nav-items').on('click', function() {
     if ($('html').width() < 850){
-      $('.nav-items').css('display', 'none');
+      $('.nav-items').hide();
     }
   });
 
   $(window).resize(function(){
     if ($('html').width() > 850){
-      $('.nav-items').css('display', '');
+      $('.nav-items').show();
     };
   });
 });
